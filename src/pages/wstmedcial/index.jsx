cf954a883314a33f2bc0086279c5c29ad5a089c1
郭文佳 <2102525175@qq.com>
2022-08-16 16:39:34 +0800

    checkOut1

--- src/pages/wstmedcial/index.jsx
+++ src/pages/wstmedcial/index.jsx
@@ -4,6 +4,7 @@ import Video from '../../components/video'
 const WestMedical = memo(() => {
   return (
     <div>
+      <span>medical DATA Json</span>
       <Video className=" top-20" url="https://sf1-hscdn-tos.pstatp.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-720p.mp4" poster="https://s2.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg" ></Video>
      <span className='ml-5'></span> WestMedical page
       </div>
